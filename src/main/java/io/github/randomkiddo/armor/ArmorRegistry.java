c562ad6fc70b28a9376180304536adde1e5af770
RandomKiddo <nghugare2@outlook.com>
2022-12-01 16:39:36 -0500

    Fixed crafting, doc comments, and language

--- src/main/java/io/github/randomkiddo/armor/ArmorRegistry.java
+++ src/main/java/io/github/randomkiddo/armor/ArmorRegistry.java
@@ -1,3 +1,11 @@
+/**
+ * The Chocolate mod, repository, and source code is licensed under the GNU GPLv3 License
+ * For more information, see: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Copyright © 2021 RandomKiddo
+ * Copyright © 2022 RandomKiddo
+ */
+
 package io.github.randomkiddo.armor;
 
 import io.github.randomkiddo.Chocolate;
@@ -9,17 +17,42 @@ import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+/**
+ * Registers all armor items
+ */
 public class ArmorRegistry {
+    /**
+     * Emerald armor material instance
+     */
     public static final ArmorMaterial EMERALD_ARMOR_MATERIAL = new EmeraldMaterial();
+    /**
+     * Physical emerald material
+     */
     public static final Item EMERALD_MATERIAL = Items.EMERALD;
+    /**
+     * Emerald helmet item
+     */
     public static final Item EMERALD_HELMET = new ArmorItem(EMERALD_ARMOR_MATERIAL, EquipmentSlot.HEAD,
             new Item.Settings().group(Chocolate.CHOCOLATE_GROUP));
+    /**
+     * Emerald chestplate item
+     */
     public static final Item EMERALD_CHESTPLATE = new ArmorItem(EMERALD_ARMOR_MATERIAL, EquipmentSlot.CHEST,
             new Item.Settings().group(Chocolate.CHOCOLATE_GROUP));
+    /**
+     * Emerald leggings item
+     */
     public static final Item EMERALD_LEGGINGS = new ArmorItem(EMERALD_ARMOR_MATERIAL, EquipmentSlot.LEGS,
             new Item.Settings().group(Chocolate.CHOCOLATE_GROUP));
+    /**
+     * Emerald boots item
+     */
     public static final Item EMERALD_BOOTS = new ArmorItem(EMERALD_ARMOR_MATERIAL, EquipmentSlot.FEET,
             new Item.Settings().group(Chocolate.CHOCOLATE_GROUP));
+    /**
+     * Registers all the items above
+     * @see Registry
+     */
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("chocolate", "emerald_helmet"), EMERALD_HELMET);
         Registry.register(Registry.ITEM, new Identifier("chocolate", "emerald_chestplate"), EMERALD_CHESTPLATE);
--- src/main/java/io/github/randomkiddo/armor/EmeraldMaterial.java
+++ src/main/java/io/github/randomkiddo/armor/EmeraldMaterial.java
@@ -15,15 +15,75 @@ import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 
+/**
+ * Defines the protocol for the material of the emerald armor
+ *
+ * Overrides:
+ * <code>
+ *     getDurability, getProtectionAmount, getEnchantability, getEquipSound,
+ *     get RepairIngredient, getName, getToughness, getKnockbackResistance
+ * </code>
+ *
+ * @see ArmorMaterial
+ */
 public class EmeraldMaterial implements ArmorMaterial {
+    /**
+     * Base durability of emerald armor
+     */
     private static final int[] BASE_DURABILITY = {600, 800, 700, 600};
+    /**
+     * Protection values of emerald armor
+     */
     private static final int[] PROTECTION_VALUES = {2, 3, 3, 2};
+
+    /**
+     * Gets a durability value given an equipment slot
+     * @param slot The given equipment slot
+     * @return The durability value
+     */
     @Override public int getDurability(EquipmentSlot slot) { return BASE_DURABILITY[slot.getEntitySlotId()]; }
+
+    /**
+     * Get the protection value given an equipment slot
+     * @param slot The given equipment slot
+     * @return The protection amount
+     */
     @Override public int getProtectionAmount(EquipmentSlot slot) { return PROTECTION_VALUES[slot.getEntitySlotId()]; }
+
+    /**
+     * Gets the enchantability factor of emerald armor
+     * @return The value 1
+     */
     @Override public int getEnchantability() { return 1; }
+
+    /**
+     * Specifies which sound to play when equipping the armor
+     * @return Default armor equip sound of diamond armor
+     */
     @Override public SoundEvent getEquipSound() { return SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND;  }
+
+    /**
+     * Gets the ingredient required to repair emerald armor
+     * @return Ingredient form of Items.EMERALD
+     * @see Items
+     */
     @Override public Ingredient getRepairIngredient() { return Ingredient.ofItems(Items.EMERALD); }
+
+    /**
+     * Fetches the name
+     * @return A string representing material name
+     */
     @Override public String getName() { return "emerald"; }
+
+    /**
+     * Gets the toughness of this armor
+     * @return A float value of 2.0F
+     */
     @Override public float getToughness() { return 2.0F; }
+
+    /**
+     * Gets the knockback resistance value of this armor
+     * @return A flot value of .02f
+     */
     @Override public float getKnockbackResistance() { return 0.02f; }
 }
