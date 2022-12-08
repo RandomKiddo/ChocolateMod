ff43f82b134329508916fa32f69a20a6ef38ac97
RandomKiddo <nghugare2@outlook.com>
2022-12-07 23:20:33 -0500

    The Essence of Chocolate

--- src/main/java/io/github/randomkiddo/chocolates/ChocolateRegistry.java
+++ src/main/java/io/github/randomkiddo/chocolates/ChocolateRegistry.java
@@ -8,8 +8,43 @@
 
 package io.github.randomkiddo.chocolates;
 
-public class ChocolateRegistry {
-    public static void register() {
+import io.github.randomkiddo.Chocolate;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
+/**
+ * Registers all actual chocolate relating items
+ */
+public class ChocolateRegistry {
+    /**
+     * Roasted cocoa beans
+     */
+    public static final Item ROASTED_COCOA_BEANS = new Item(new FabricItemSettings().group(Chocolate.CHOCOLATE_GROUP));
+    /**
+     * Cocoa butter
+     */
+    public static final Item COCOA_BUTTER = new Item(new FabricItemSettings().group(Chocolate.CHOCOLATE_GROUP));
+    /**
+     * Chocolate liquor
+     */
+    public static final Item CHOCOLATE_LIQUOR = new Item(new FabricItemSettings().group(Chocolate.CHOCOLATE_GROUP));
+    /**
+     * Chocolate
+     */
+    public static final Item CHOCOLATE = new Item(new FabricItemSettings().group(Chocolate.CHOCOLATE_GROUP)
+            .food(new FoodComponent.Builder().hunger(4).saturationModifier(6f).snack().build()));
+
+    /**
+     * Registers all the items above
+     * @see Registry
+     */
+    public static void register() {
+        Registry.register(Registry.ITEM, new Identifier("chocolate", "roasted_cocoa_beans"), ROASTED_COCOA_BEANS);
+        Registry.register(Registry.ITEM, new Identifier("chocolate", "cocoa_butter"), COCOA_BUTTER);
+        Registry.register(Registry.ITEM, new Identifier("chocolate", "chocolate_liquor"), CHOCOLATE_LIQUOR);
+        Registry.register(Registry.ITEM, new Identifier("chocolate", "chocolate"), CHOCOLATE);
     }
 }
