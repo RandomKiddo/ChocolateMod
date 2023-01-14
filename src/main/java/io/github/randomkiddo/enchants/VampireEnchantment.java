6fc1e4820673bb4f0b07bf1ad8bcb897d1ac174b
RandomKiddo <nghugare2@outlook.com>
2023-01-13 22:36:21 -0500

    Update Copyright

--- src/main/java/io/github/randomkiddo/enchants/VampireEnchantment.java
+++ src/main/java/io/github/randomkiddo/enchants/VampireEnchantment.java
@@ -3,7 +3,8 @@
  * For more information, see: https://www.gnu.org/licenses/gpl-3.0.en.html
  *
  * Copyright © 2021 RandomKiddo
- * Copyright © 2022 RandomKiddo
+ * Copyright © 2022 RandomKiddo, danield33
+ * Copyright © 2023 RandomKiddo
  */
 
 package io.github.randomkiddo.enchants;
