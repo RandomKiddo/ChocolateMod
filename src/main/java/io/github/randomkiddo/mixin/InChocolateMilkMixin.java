cf916675904971d371a4b188116ca91eec3cf67d
Mag1cmang0 <120691005+Mag1cmang0@users.noreply.github.com>
2023-01-18 17:49:09 -0500

    Added Chocolate Milk

---
+++ src/main/java/io/github/randomkiddo/mixin/InChocolateMilkMixin.java
@@ -0,0 +1,46 @@
+/**
+ * The Chocolate mod, repository, and source code is licensed under the GNU GPLv3 License
+ * For more information, see: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Copyright © 2021 RandomKiddo
+ * Copyright © 2022 RandomKiddo, danield33
+ * Copyright © 2023 RandomKiddo, danield33, NithilB, pranavmoola, Mag1cmang0
+ */
+
+package io.github.randomkiddo.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * InAcidMixin is a spongepowered mixin for Minecraft that detects if an entity is in acid, and if so,
+ * the entity is set to take damage
+ *
+ * Injected into <code>isTouchingWater</code> at <code>RETURN</code>
+ * Non-cancellable
+ *
+ * @see Mixin
+ * @see Inject
+ * @see At
+ * @see Entity
+ */
+@Mixin(Entity.class)
+public class InChocolateMilkMixin {
+    /**
+     * Injects the specified code into isTouchingWater
+     * @param cir The callback info of the method and its returned boolean value
+     * @see Entity
+     */
+    @Inject(method="isTouchingWater()Z", at=@At("RETURN"), cancellable = false)
+    private void mixin(CallbackInfoReturnable<Boolean> cir) {
+        Entity entity = (Entity)(Object)this;
+        String name = entity.getBlockStateAtPos().getBlock().getName().toString();
+        if (cir.getReturnValue() && name.contains("block.chocolate.chocolate_milk")) {
+            entity.damage(DamageSource.FALLING_STALACTITE, 100.0f);
+        }
+    }
+}
