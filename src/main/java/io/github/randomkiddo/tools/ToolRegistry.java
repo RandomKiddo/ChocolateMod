afebecfc8f0a38ceb74b6f542c9143e5ce50346a
Nithil_B <77587413+NithilB@users.noreply.github.com>
2023-03-17 17:36:09 -0400

    Update ToolRegistry.java

--- src/main/java/io/github/randomkiddo/tools/ToolRegistry.java
+++ src/main/java/io/github/randomkiddo/tools/ToolRegistry.java
@@ -14,6 +14,8 @@ import net.minecraft.item.ToolItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import javax.tools.Tool;
+
 /**
  * Registers all tools for this mod
  */
@@ -23,12 +25,14 @@ public class ToolRegistry {
      */
     public static ToolItem FOSELIUM_PICKAXE = new FoseliumPickaxeItem(FoseliumMaterial.INSTANCE, 2, -2.8F, new Item.Settings().group(Chocolate.CHOCOLATE_GROUP));
 
-    /**
+    public static ToolItem ETHERIUM_SWORD = new EtheriumSwordItem(EtheriumMaterial.INSTANCE, 6, -2.4F, new Item.Settings().group(Chocolate.CHOCOLATE_GROUP));
+    /**ium
      * Registers all the tool instances above
      * @see Registry
      */
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("chocolate", "foselium_pickaxe"), FOSELIUM_PICKAXE);
+        Registry.register(Registry.ITEM,  new Identifier("chocolate", "etherium_sword"), ETHERIUM_SWORD);
 
     }
 }
