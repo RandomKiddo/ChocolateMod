79d71d5b4926b19f5576d34514556255d54d802d
RandomKiddo <nghugare2@outlook.com>
2023-03-19 23:00:38 -0400

    Documentation Comments

--- src/main/java/io/github/randomkiddo/entities/EntityRegistry.java
+++ src/main/java/io/github/randomkiddo/entities/EntityRegistry.java
@@ -1,3 +1,12 @@
+/**
+ * The Chocolate mod, repository, and source code is licensed under the GNU GPLv3 License
+ * For more information, see: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Copyright © 2021 RandomKiddo
+ * Copyright © 2022 RandomKiddo, danield33
+ * Copyright © 2023 RandomKiddo, danield33, NithilB, pranavmoola, Mag1cmang0
+ */
+
 package io.github.randomkiddo.entities;
 
 import io.github.randomkiddo.Chocolate;
@@ -15,14 +24,27 @@ import net.minecraft.util.registry.Registry;
 
 import static software.bernie.example.registry.RegistryUtils.registerItem;
 
+/**
+ * Registers all entity-related items for this mod
+ */
 public class EntityRegistry {
+    /**
+     * Evil Chicken Entity
+     */
     public static final EntityType<EvilChickenEntity> EVIL_CHICKEN = Registry.register(
             Registry.ENTITY_TYPE, new Identifier("chocolate", "evil_chicken"),
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, EvilChickenEntity::new)
                     .dimensions(EntityDimensions.fixed(0.5f, 0.5f)).build());
+    /**
+     * Evil Chicken Entity Spawn Egg
+     */
     public static final Item EVIL_CHICKEN_SPAWN_EGG = registerItem("evil_chicken_spawn_egg",
             new SpawnEggItem(EntityRegistry.EVIL_CHICKEN, 0x948e8d, 0x3b3635,
                     new FabricItemSettings().group(Chocolate.CHOCOLATE_GROUP).maxCount(1)));
+    /**
+     * Registers all entity attributes
+     * @see Registry
+     */
     public static void register() {
         FabricDefaultAttributeRegistry.register(EntityRegistry.EVIL_CHICKEN, EvilChickenEntity.setAttributes());
     }
--- src/main/java/io/github/randomkiddo/entities/custom/EvilChickenEntity.java
+++ src/main/java/io/github/randomkiddo/entities/custom/EvilChickenEntity.java
@@ -1,3 +1,12 @@
+/**
+ * The Chocolate mod, repository, and source code is licensed under the GNU GPLv3 License
+ * For more information, see: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Copyright © 2021 RandomKiddo
+ * Copyright © 2022 RandomKiddo, danield33
+ * Copyright © 2023 RandomKiddo, danield33, NithilB, pranavmoola, Mag1cmang0
+ */
+
 package io.github.randomkiddo.entities.custom;
 
 import net.minecraft.block.BlockState;
@@ -22,20 +31,45 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
+/**
+ * Evil Chicken Entity Class; Suppresses Deprecation Warnings
+ * @see HostileEntity
+ * @see IAnimatable
+ */
 @SuppressWarnings({ "deprecated" })
 public class EvilChickenEntity extends HostileEntity implements IAnimatable {
+    /**
+     * The animation factory
+     */
     @SuppressWarnings({ "deprecated" })
     private AnimationFactory factory = new AnimationFactory(this);
 
+    /**
+     * Constructs this entity
+     * @param entityType The type of entity
+     * @param world The world to live in
+     */
     public EvilChickenEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
 
+    /**
+     * Creates a child on a server (unused, Deprecated)
+     * @param world The world to perform this action on
+     * @param entity The entity creating a child
+     * @return A child HostileEntity instance
+     */
     @Nullable
+    @Deprecated
     public HostileEntity createChild(ServerWorld world, HostileEntity entity) {
         return null;
     }
 
+    /**
+     * Sets the entity attributes
+     * @return A builder with all the entity attributes
+     * @see DefaultAttributeContainer.Builder
+     */
     public static DefaultAttributeContainer.Builder setAttributes() {
         return HostileEntity.createHostileAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0D)
@@ -44,12 +78,23 @@ public class EvilChickenEntity extends HostileEntity implements IAnimatable {
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0f);
     }
 
+    /**
+     * Initializes the goals for the entity
+     */
     protected void initGoals() {
         this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
         this.goalSelector.add(8, new LookAroundGoal(this));
         this.targetSelector.add(2, new ActiveTargetGoal(this, PlayerEntity.class, true));
     }
 
+    /**
+     * The play state predicate for the entity; Suppresses deprecated warnings
+     * @param event The event to work on
+     * @return A PlayState instance
+     * @param <E> Some object E extending IAnimatable; The Entity
+     * @see IAnimatable
+     * @see PlayState
+     */
     @SuppressWarnings({ "deprecated" })
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (event.isMoving()) {
@@ -61,35 +106,63 @@ public class EvilChickenEntity extends HostileEntity implements IAnimatable {
         return PlayState.CONTINUE;
     }
 
+    /**
+     * Registers the entity controllers
+     * @param animationData The entity's animation data
+     */
     @Override
     public void registerControllers(AnimationData animationData) {
         animationData.addAnimationController(new AnimationController(this, "controller",
                 0, this::predicate));
     }
 
+    /**
+     * Gets the animation factory
+     * @return The factory field
+     * @see AnimationFactory
+     */
     @Override
     public AnimationFactory getFactory() {
         return factory;
     }
 
+    /**
+     * Gets the ambient sound for the entity
+     * @return SoundEvents.ENTITY_CHICKEN_AMBIENT
+     * @see SoundEvents
+     */
     @Override
     protected SoundEvent getAmbientSound() {
         return SoundEvents.ENTITY_CHICKEN_AMBIENT;
     }
 
+    /**
+     * Gets the hurt sound for the entity
+     * @param source The source of the damage
+     * @return SoundEvents.ENTITY_CHICKEN_HURT
+     * @see SoundEvents
+     */
     @Override
     protected SoundEvent getHurtSound(DamageSource source) {
         return SoundEvents.ENTITY_CHICKEN_HURT;
     }
-
+    /**
+     * Gets the death sound for the entity
+     * @return SoundEvents.ENTITY_CHICKEN_DEATH
+     * @see SoundEvents
+     */
     @Override
     protected SoundEvent getDeathSound() {
         return SoundEvents.ENTITY_CHICKEN_DEATH;
     }
 
+    /**
+     * Plays the entity's step sound
+     * @param pos The position to play the sound
+     * @param state The state of the block at pos
+     */
     @Override
     protected void playStepSound(BlockPos pos, BlockState state) {
         this.playSound(SoundEvents.ENTITY_CHICKEN_STEP, 0.15f, 1.0f);
     }
-
 }
